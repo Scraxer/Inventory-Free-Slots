738d26298308e2c2729c3b36132a1330d710ac1f
Scraxer <c.jennings@oceanfire.co.nz>
2024-09-10 16:11:02 +1200

    Update InventoryFreeSlotsPlugin.java

--- src/main/java/com/scraxer/InventoryFreeSlotsPlugin.java
+++ src/main/java/com/scraxer/InventoryFreeSlotsPlugin.java
@@ -1,1 +1,33 @@
+package com.scraxer;
+
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
+import javax.inject.Inject;
+
+@PluginDescriptor(
+    name = "Inventory Free Slots",
+    description = "Displays the number of free inventory slots on the inventory bag icon",
+    tags = {"inventory", "slots", "ui"}
+)
+public class InventoryFreeSlotsPlugin extends Plugin
+{
+    @Inject
+    private OverlayManager overlayManager;
+
+    @Inject
+    private InventoryFreeSlotsOverlay overlay;
+
+    @Override
+    protected void startUp() throws Exception
+    {
+        overlayManager.add(overlay);
+    }
+
+    @Override
+    protected void shutDown() throws Exception
+    {
+        overlayManager.remove(overlay);
+    }
+}
 
