725e8af5c4ce2d4a9a29dfb7f720be7fddef9be4
Scraxer <c.jennings@oceanfire.co.nz>
2024-09-10 21:20:21 +1200

    Update InventoryFreeSlotsPlugin.java

--- src/main/java/com/scraxer/InventoryFreeSlotsPlugin.java
+++ src/main/java/com/scraxer/InventoryFreeSlotsPlugin.java
@@ -1,14 +1,19 @@
 package com.scraxer;
 
+import com.google.inject.Provides;
+import net.runelite.api.events.ItemContainerChanged;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
+
 import javax.inject.Inject;
 
 @PluginDescriptor(
-    name = "Inventory Free Slots",
-    description = "Displays the number of free inventory slots on the inventory bag icon",
-    tags = {"inventory", "slots", "ui"}
+        name = "Inventory Free Slots",
+        description = "Displays the number of free inventory slots on the inventory bag icon",
+        tags = {"inventory", "free", "slots", "display", "ui"}
 )
 public class InventoryFreeSlotsPlugin extends Plugin
 {
@@ -16,18 +21,34 @@ public class InventoryFreeSlotsPlugin extends Plugin
     private OverlayManager overlayManager;
 
     @Inject
-    private InventoryFreeSlotsOverlay overlay;
+    private InventoryFreeSlotsOverlay inventoryFreeSlotsOverlay;
 
     @Override
     protected void startUp() throws Exception
     {
-        overlayManager.add(overlay);
+        overlayManager.add(inventoryFreeSlotsOverlay);
     }
 
     @Override
     protected void shutDown() throws Exception
     {
-        overlayManager.remove(overlay);
+        overlayManager.remove(inventoryFreeSlotsOverlay);
+    }
+
+    @Provides
+    InventoryFreeSlotsConfig provideConfig(ConfigManager configManager)
+    {
+        return configManager.getConfig(InventoryFreeSlotsConfig.class);
+    }
+
+    // Subscribe to inventory changes to update free slots
+    @Subscribe
+    public void onItemContainerChanged(ItemContainerChanged event)
+    {
+        // Trigger an update when the inventory changes
+        if (event.getContainerId() == net.runelite.api.InventoryID.INVENTORY.getId())
+        {
+            inventoryFreeSlotsOverlay.updateFreeSlots();
+        }
     }
 }
-
